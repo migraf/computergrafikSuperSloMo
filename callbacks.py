4cedcc7a65705d0b345d2eb268f68dd2e159b762
migraf <michael.m.graf@gmx.de>
2019-01-23 11:35:48 +0100

    tensor printing

--- callbacks.py
+++ callbacks.py
@@ -28,6 +28,7 @@ class FlowVisualisationCallback(Callback):
 
     def _trigger_epoch(self):
             for tensor in self.tensors:
+                print(type(tensor))
                 flow = self.visualize_flow(tensor.eval(session=self.trainer.sess))
                 self.trainer.monitors.put_image("flow", flow)
 
