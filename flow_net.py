da2f50f9c31492268223d434b68ca73b5dd4bdfa
Michael Graf <michael.m.graf@gmx.de>
2019-03-07 11:45:15 +0100

    added queue input and staging input as well as reduced number of steps by the number of gpus

--- flow_net.py
+++ flow_net.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
     # Steps at which to increase the learning rate
 
 
-    model = FlowNetModel("flownet", df1.height, df1.width, int(args.num_batches))
+    model = FlowNetModel("flownet", df.height, df.width, int(args.num_batches))
     lr_increase_schedule = [(10000, 1e-4), (300000, (1e-4)/2), (400000, (1e-4)/4), (500000, (1e-4)/8)]
     config = TrainConfig(
         model=model,
