ba4fc69ae960efbc4139d30163f4673ff3ad80ec
Michael Graf <michael.m.graf@gmx.de>
2019-03-15 12:55:18 +0100

    flow viz in util

--- flow_net.py
+++ flow_net.py
@@ -236,7 +236,7 @@ def inference(saved_model, left_image_path, right_image_path, gt_flow=None):
 
     predictor = OfflinePredictor(predict_config)
 
-    flow_prediction = predictor(left_image, right_image)
+    flow_prediction = predictor(left_image, right_image) * 10
     print("Max flow predict : {}".format(np.max(flow_prediction)))
     print("Min flow predict : {}".format(np.min(flow_prediction)))
     print(flow_prediction[0].shape)
