087fd42839295f62ccae718f1aecd2f53ce2f4aa
Michael Graf <michael.m.graf@gmx.de>
2019-03-15 13:22:04 +0100

    flow viz in util

--- flow_net.py
+++ flow_net.py
@@ -241,7 +241,7 @@ def inference(saved_model, left_image_path, right_image_path, gt_flow=None):
     print(flow_prediction[0].shape)
     flow_viz = visualize_flow(flow_prediction[0] * 10)
 
-    print("Epe error: {}".format(tf.norm(flow_prediction - read_flow(gt_flow), axis=3)))
+    print("Epe error: {}".format(tf.reduce_mean(tf.norm(flow_prediction - read_flow(gt_flow), axis=3))))
     print("Max flow predict : {}".format(np.max(flow_viz)))
     print("Min flow predict : {}".format(np.min(flow_viz)))
 
